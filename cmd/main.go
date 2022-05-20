50049c6f34e1bba9469ce82e9f1cfad183b36d44
Devin Collins <me@imdevinc.com>
2022-05-20 15:23:38 -0700

    Examplse

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strings"
 
 	fifa "github.com/ImDevinC/go-fifa"
 )
@@ -10,18 +12,43 @@ const (
 	competitionId string = "17" // 17 world cup
 )
 
+func showCompetitions(client *fifa.Client) error {
+	comps, err := client.GetCompetitions()
+	if err != nil {
+		return err
+	}
+	for _, c := range comps {
+		log.Printf("Id: %s", c.CompetitionId)
+		log.Printf("Name: %s", c.Name[0].Description)
+	}
+	return nil
+}
+
+func getCompetitionsByName(client *fifa.Client, input string) error {
+	comps, err := client.GetCompetitions()
+	if err != nil {
+		return err
+	}
+	input = strings.ToLower(input)
+	var results []fifa.CompetitionResponse
+	for _, c := range comps {
+		name := strings.ToLower(c.Name[0].Description)
+		if strings.Contains(name, input) {
+			results = append(results, c)
+		}
+	}
+	if len(results) > 1 {
+		fmt.Println("Multiple tournaments returned")
+		fmt.Printf("%+v\n", results)
+	} else if len(results) == 0 {
+		fmt.Println("Found " + results[0].Name[0].Description)
+	} else {
+		fmt.Println("No results found")
+	}
+	return nil
+}
 func main() {
-	client := fifa.Client{}
-	matches, err := client.GetCurrentMatches()
-	if err != nil {
-		panic(err)
-	}
-	for _, m := range matches {
-		fmt.Printf("CompetitionId: %s, SeasonId: %s, StageId: %s, MatchId: %s\n", m.CompetitionId, m.SeasonId, m.StageId, m.Id)
-	}
-	comp, err := client.GetCompetition(&fifa.GetCompetitionsOptions{CompetitionId: competitionId})
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("%+v", comp)
+	client := &fifa.Client{}
+	// showCompetitions(client)
+	getCompetitionsByName(client, "world cup")
 }
