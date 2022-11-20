f0ee17f73399e8ab4aa3cc6f3408d0205a668828
Devin Collins <me@imdevinc.com>
2022-11-20 08:27:12 -0800

    remove invalid endpoint

--- seasons.go
+++ seasons.go
@@ -15,19 +15,3 @@ func (c *Client) GetSeason(options *GetSeasonOptions) (*SeasonResponse, error) {
 	}
 	return &season, nil
 }
-
-type GetSeasonStandingsOptions struct {
-	SeasonId      string
-	CompetitionId string
-	StageId       string
-}
-
-func (c *Client) GetSeasonStandings(opts *GetSeasonStandingsOptions) ([]StandingsResult, error) {
-	var response StandingResponse
-	url := fmt.Sprintf("/calendar/%s/%s/%s/Standing", opts.CompetitionId, opts.SeasonId, opts.StageId)
-	_, err := c.get(url, &response, nil)
-	if err != nil {
-		return nil, err
-	}
-	return response.Results, nil
-}
--- seasons_test.go
+++ seasons_test.go
@@ -17,22 +17,3 @@ func TestGetSeason(t *testing.T) {
 		t.FailNow()
 	}
 }
-
-func TestGetSeasonStandings(t *testing.T) {
-	t.Parallel()
-	client := fifa.Client{}
-	resp, err := client.GetSeasonStandings(&fifa.GetSeasonStandingsOptions{
-		CompetitionId: "2000001049",
-		SeasonId:      "400198457",
-		StageId:       "400198931",
-	})
-	if ok := assert.Nil(t, err, "expected no error with GetSeasonStandings, got: %s", err); !ok {
-		t.FailNow()
-	}
-	if ok := assert.Greater(t, len(resp), 0, "expected more than one result, got none"); !ok {
-		t.FailNow()
-	}
-	if ok := assert.Equal(t, resp[0].CompetitionId, "2000001049", "incorrect competition id"); !ok {
-		t.FailNow()
-	}
-}
